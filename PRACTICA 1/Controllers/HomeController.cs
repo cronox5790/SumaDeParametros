b9d334d8c7e2560a25f2a6edc1e4f043e8780375
171G0574@rcarbonifera.tecnm.mx <171G0574@rcarbonifera.tecnm.mx>
2020-09-15 17:42:15 -0500

    Actividad1

---
+++ PRACTICA 1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PRACTICA_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace PRACTICA_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Captura()
+        {
+            return View();
+        }
+
+        public IActionResult Resultado(Valores x)
+        {
+            var resultados = x.Valor1 + x.Valor2;
+            return View(resultados);
+        }
+    }
+}
---
+++ PRACTICA 1/Models/Valores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PRACTICA_1.Models
+{
+    public class Valores
+    {
+          
+       public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+    }
+}
